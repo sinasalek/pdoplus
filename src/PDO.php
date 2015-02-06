4155fd49bf16642160d7b193cb06a45c9ed5de02
Sina Salek <sinasalek@gmail.com>
2015-02-06 18:33:46 +0330

    Update PDO.php
    nested transaction support for PDO

--- src/PDO.php
+++ src/PDO.php
@@ -2360,4 +2360,52 @@ class PDO extends DB {
     return $queries;
   }
 
+
+  // Database drivers that support SAVEPOINTs.
+  protected static $savepointTransactions = array("pgsql", "mysql");
+// The current transaction level.
+  protected $transLevel = 0;
+  protected function nestable() {
+    return in_array($this->getAttribute(PDO::ATTR_DRIVER_NAME),
+      self::$savepointTransactions);
+  }
+  function transaction($call) {
+    $this->beginTransaction();
+    try {
+      $ret = call_user_func($call);
+    } catch(Exception $e) {
+      $this->rollBack();
+      throw $e;
+    }
+    if($ret) {
+      $this->commit();
+    } else {
+      $this->rollBack();
+    }
+    return $ret;
+  }
+  public function beginTransaction() {
+    if(!$this->nestable() || $this->transLevel == 0) {
+      parent::beginTransaction();
+    } else {
+      $this->exec("SAVEPOINT LEVEL{$this->transLevel}");
+    }
+    $this->transLevel++;
+  }
+  public function commit() {
+    $this->transLevel--;
+    if(!$this->nestable() || $this->transLevel == 0) {
+      parent::commit();
+    } else {
+      $this->exec("RELEASE SAVEPOINT LEVEL{$this->transLevel}");
+    }
+  }
+  public function rollBack() {
+    $this->transLevel--;
+    if(!$this->nestable() || $this->transLevel == 0) {
+      parent::rollBack();
+    } else {
+      $this->exec("ROLLBACK TO SAVEPOINT LEVEL{$this->transLevel}");
+    }
+  }
 }
